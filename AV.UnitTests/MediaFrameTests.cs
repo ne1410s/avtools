e7b13a8de07d4408b2a368cb37c76b880930f679
ne1410s <pgj646@gmail.com>
2021-02-28 11:02:02 +0000

    Minor test file.

---
+++ AV.UnitTests/MediaFrameTests.cs
@@ -0,0 +1,52 @@
+﻿using AV.Core;
+using AV.Core.Container;
+using AV.Source;
+using FFmpeg.AutoGen;
+using Xunit;
+
+namespace AV.UnitTests
+{
+    public class MediaFrameTests
+    {
+        private static readonly byte[] TestKey = new byte[] { 3, 44, 201, 0, 6 };
+    
+        public MediaFrameTests()
+        {
+            Library.FFmpegDirectory = "ffmpeg";
+            Library.FFmpegLoadModeFlags = FFmpegLoadMode.FullFeatures;
+            Library.LoadFFmpeg();
+        }
+
+        [Theory]
+        //[InlineData(@"C:\temp\vid-test\1.3gp")]
+        [InlineData(@"C:\temp\vid-test\1.avi")]
+        //[InlineData(@"C:\temp\vid-test\1.flv")]
+        //[InlineData(@"C:\temp\vid-test\1.m4v")]
+        //[InlineData(@"C:\temp\vid-test\1.mkv")]
+        //[InlineData(@"C:\temp\vid-test\1.mov")]
+        //[InlineData(@"C:\temp\vid-test\1.mp4")]
+        //[InlineData(@"C:\temp\vid-test\1.mpeg")]
+        //[InlineData(@"C:\temp\vid-test\1.mpg")]
+        //[InlineData(@"C:\temp\vid-test\1.mts")]
+        //[InlineData(@"C:\temp\vid-test\1.vob")]
+        //[InlineData(@"C:\temp\vid-test\1.webm")]
+        //[InlineData(@"C:\temp\vid-test\1.wmv")]
+        public void FileSource_LoadsMediaInfo(string path)
+        {
+            // Arrange
+            using var source = new FileSource(path);
+            using var container = new MediaContainer(source);
+            container.Initialize();
+            container.Open();
+
+            var snaps = 20;
+            var gap = container.MediaInfo.Duration / snaps;
+            for (var i = 0; i < snaps; i++)
+            {
+                var frame = container.Seek(gap * i);
+                //var x = container.Read();
+                
+            }
+        }
+    }
+}
