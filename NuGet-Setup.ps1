d26c058b2cfe6acfa5c13e571c8ed8ae33c1b894
ne1410s <pgj646@gmail.com>
2021-03-11 20:05:46 +0000

    nuget stuff

--- NuGet-Setup.ps1
+++ NuGet-Setup.ps1
@@ -3,3 +3,6 @@ nuget config -set DefaultPushSource="$env:USERPROFILE/.nuget-tool/packages"
 
 # add repo source to package manager
 nuget sources add -name localrepo -source "$env:USERPROFILE/.nuget-tool/packages"
+
+# AV.Extensions gets packed on build, so just run:
+nuget push .\AV.Extensions\bin\Release\AV.Extensions.*.nupkg
